868146e2e3f5c698c8b96b768cc14f365f22801c
Pufikas <dominykas.guzys@gmail.com>
2023-01-20 19:09:42 +0200

    apple

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ let e = t.split(/^\n$/gm)
 let D = Date.now()
 let x = 0
 
-const play = require("audio-play")
-const load = require("audio-loader")  
-load("./bad_apple.mp3").then(play)
-
-
 const c = (t) => {
+  sound.play("./bad_apple.mp3")
   let a = t.split(/\d$/gm)
   let p = ++x * (1/30) * 1000
   let D2 = Date.now() - D
   console.log(a[2]),
   (s = Date.now()),
-  load("./bad_apple.mp3").then(play),
   setTimeout(() => {
     console.clear()
     e.length > 0 ? c(e.shift()) : console.log("End of the line")
